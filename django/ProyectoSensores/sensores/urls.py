e5ebd0c5ddbb8bffc2eb5cd14fecc7d3464fdde7
Marco-uwu <mmaldonadof1900@alumno.ipn.mx>
2024-11-04 10:39:01 -0600

    Eliminados directorios innecesarios

--- django/ProyectoSensores/sensores/urls.py
+++
@@ -1,7 +0,0 @@
-# sensores/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.mostrar_tabla, name='mostrar_tabla'),
-]
--- django/ProyectoSensores/sensores/views.py
+++
@@ -1,7 +0,0 @@
-# sensores/views.py
-from django.shortcuts import render
-from .models import SensorData
-
-def mostrar_tabla(request):
-    datos = SensorData.objects.all()
-    return render(request, 'sensores/tabla.html', {'datos': datos})
