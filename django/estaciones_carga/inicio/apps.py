390c8ba7f199b21a8a862ababffd2eeafb1dd34d
Marco-uwu <mmaldonadof1900@alumno.ipn.mx>
2024-11-23 17:46:32 -0600

    Eliminado directorio django, para reemplazar por un repositorio nuevo

--- django/estaciones_carga/inicio/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-class InicioConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'inicio'
-    def ready(self):
-        import inicio.signals
